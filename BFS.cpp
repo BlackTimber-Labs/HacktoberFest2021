f70c0f079d15ca853bbe86a85ebc2627127e8166
Saumya Jaiswal <72287681+Saumyaj100300@users.noreply.github.com>
2021-10-27 16:33:04 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+#include <chrono>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+using namespace chrono;
+
+//template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag ,  tree_order_statistics_node_update >;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define read(x)          \
+    for (auto &inps : x) \
+    cin >> inps
+#define all(v) v.begin(), v.end()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...)       \
+    F_ORC(__VA_ARGS__) \
+    (__VA_ARGS__)
+
+const int mod = 1e9 + 7;
+const int smod = 1e5 + 1;
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+bool isPowerOfTwo(ll n)
+{
+    return !(n && (n & (n - 1)));
+}
+int setBits(ll n)
+{
+    ll ans = 0;
+    while (n > 0)
+    {
+        n = (n & (n - 1));
+        ans++;
+    }
+    return ans;
+}
+
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+vector<int> adj[smod];
+vector<bool> vis(smod, false);
+
+//<---------GRAPH ALOGORITHMS------------>
+void bfs(vi &bfsvector, int start)
+{
+    for (int i = 0; i < smod; i++)
+    {
+        vis[i] = false;
+    }
+
+    queue<int> q;
+    q.push(start);
+    bfsvector.pb(start);
+    vis[start] = true;
+    while (!q.empty())
+    {
+        int a = q.front();
+        q.pop();
+        for (auto x : adj[a])
+        {
+            if (!vis[x])
+            {
+                q.push(x);
+                bfsvector.pb(x);
+                vis[x] = true;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    vector<int> bfsv;
+    bfs(bfsv, 1);
+    for (auto i : bfsv)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+    init_code();
+    auto start = high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    float timeCount = duration.count();
+    timeCount /= 1000000;
+    debug("Time Taken", timeCount);
+    return 0;
+}
